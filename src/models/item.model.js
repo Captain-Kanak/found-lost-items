1712db32f648c408701c3e2d0fae464b56ac70d3
Captain-Kanak <kanakroy835@gmail.com>
2025-10-21 13:12:35 +0600

    added: mongodb, mongoose and item, user model

---
+++ src/models/item.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// Define the item schema
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+// Create the item model
+const Item = mongoose.model("Item", itemSchema);
+
+// Export the item model
+export default Item;
